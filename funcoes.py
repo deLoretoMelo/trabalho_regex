caf6cdd92ae0cff9de19bfbd586833cd6b91aa23
deLoretoMelo <meloloretomarcelo@gmail.com>
2024-09-15 13:21:35 -0300

    primeiro commit

---
+++ funcoes.py
@@ -0,0 +1,161 @@
+# Alunos:
+# Eduardo Fernandes Albuquerque
+# João Pedro Vasconcelos de Lima
+# Ladielma Carina Santos Teixeira
+# Marcelo Malcher Gillet de Loreto Melo
+# Virgilio Cardoso Dantas Neto
+
+import re
+
+def teste_estresse (lista_teste, funcao):
+    # Função que vai fazer o teste de estresse com as respostas de aceitação ou não das cadeias
+    print("")
+    print("")
+    for a in lista_teste:
+        # Formatação do print no console
+        if(funcao(a)):
+            print(f'"{a}" É uma sentença válida')
+        else:
+           print(f'"{a}" Não é uma sentença válida')
+    print("")
+    print("")
+
+# Primeira questão (Máscaras de validação)
+def validar_nome(nome):
+    # A função recebe um uma string que contem um nome, e o regex verifica se o nome está no formato correto
+    # O nome deve começar com uma letra maiúscula, e pode ter até 3 palavras (nome do meio é opcional), cada uma começando com letra maiúscula
+    # Cada nome deve ser separa por um espaço em branco
+    regex = re.compile(r"^([A-Z][a-z]*\s){,2}[A-Z][a-z]*$")
+
+    validacao = regex.fullmatch(nome)
+
+    return bool(validacao)
+
+def validar_email(email):
+    # A função recebe um uma string que contem um email, e o regex verifica se o email está no formato correto
+    # A cadeia deve conter apenas letras minusculas, um símbolo @ e terminar com .br ou .com.br
+    # O @ deve estar entre as letras minusculas e deve-se ter no mínimo duas (ex: a@b.com.br)
+    regex = re.compile(r"^[a-z]+@[a-z]+\.((com\.)?)br$")
+
+    validacao = regex.fullmatch(email)
+
+    return bool(validacao)
+
+def validar_senha(senha):
+    # A função recebe um uma string que contem uma senha, e o regex verifica se a senha está no formato correto
+    # A cadeia deve possuir comprimento 8 e conter pelo menos uma letra maiúscula e um número
+    regex = re.compile(r"(?=.*[A-Z])(?=.*\d)[A-Za-z0-9]{8}")
+
+    validacao = regex.fullmatch(senha)
+
+    return bool(validacao)
+
+def validar_cpf(cpf):
+    # A função recebe um uma string que contem um cpf, e o regex verifica se o cpf está no formato correto
+    # O cpf deve atender ao formato "xxx.xxx.xxx-xx", sendo o cpf digitado existente ou não
+    regex = re.compile(r"(\d{3}\.){2}\d{3}[-]\d{2}")
+
+    validacao = regex.fullmatch(cpf)
+
+    return bool(validacao)
+
+def validar_numero(telefone):
+    # A função recebe um uma string que contem um número de telefone, e o regex verifica se o número está no formato correto
+    # O número deve atender a um dos seguintes formatos: (xx) 9xxxx-xxxx, (xx) 9xxxxxxxx ou xx 9xxxxxxxx
+    # Assim como o cpf, o número tem que estar dentro do formato, mas não precisa ser um número existente
+    regex = re.compile(r"(\(\d{2}\)\s9\d{4}[-]\d{4}|\(\d{2}\)\s9\d{8}|\d{2}\s9\d{8})")
+
+    validacao = regex.fullmatch(telefone)
+
+    return bool(validacao)
+
+def validar_data(data):
+    # A função recebe um uma string que contem uma data, e o regex verifica se a data está no formato correto
+    # A data deve atender ao formato "dd/mm/aaaa hh:mm:ss"
+    # A data não precisa ser válida, apenas estar no formato correto
+    regex = re.compile(r"(\d{2}/){2}\d{4}\s(\d{2}:){2}\d{2}")
+
+    validacao = regex.fullmatch(data)
+
+    return bool(validacao)
+
+def validar_numero_real(num):
+    # A função recebe um uma string que contem um número real, e o regex verifica se o número está no formato correto
+    # Pode ou não conter os símbolos "+" ou "-", e deve conter pelo menos um número
+    regex = re.compile(r"(\+|\-)?\d+(.\d+)?")
+
+    validacao = regex.fullmatch(num)
+
+    return bool(validacao)
+
+# Segunda questão (Arranjos familiares)
+def a(arranjo):
+    # O regex verifica se o arranjo familiar é um casal heterossexual com pelo menos duas filhas ou pelo menos um filho
+    regex = re.compile(r"(HM|MH)((h*m+h*m+(h|m)*|m*h+m*(h|m)*))")
+    
+    validacao = regex.fullmatch(arranjo)
+
+    return bool(validacao)
+
+def b(arranjo):
+    # O regex verifica se o arranjo familiar é um casal heterossexual com número ímpar de filhas
+    regex = re.compile(r"(HM|MH)(h*m(h*mh*m)*h*)")
+
+    validacao = regex.fullmatch(arranjo)
+
+    return bool(validacao)
+
+def c(arranjo):
+    # O regex verifica se o arranjo familiar é um casal heterossexual em que a filha mais velha é mulher e o mais novo é homem
+    regex = re.compile(r"(HM|MH)(m(m|h)*h)")
+
+    validacao = regex.fullmatch(arranjo)
+
+    return bool(validacao)
+
+def d(arranjo):
+    # O regex verifica se o arranjo familiar é um casal homossexual com pelo menos seis filhos
+    # Os dois primeiros filhos e os dois últimos filhos devem formar um casal
+    regex = re.compile(r"(MM|HH)(mh|hm)(m|h){2}(m|h)*(mh|hm)")
+
+    validacao = regex.fullmatch(arranjo)
+
+    return bool(validacao)
+
+def e(arranjo):
+    # O regex verifica se o arranjo familiar é um casal homossexual em que o sexo dos filhos é alternada
+    # A expressão não tá considerando que o casal não tenha filhos
+    regex = re.compile(r"(MM|HH)((m(hm)*h?)|(h(mh)*m?))")
+
+    validacao = regex.fullmatch(arranjo)
+
+    return bool(validacao)
+
+def f(arranjo):
+    # O regex verifica se o arranjo familiar é um casal homossexual com qualquer número de filhos
+    # A expressão não pode ter dois filhos homens consecutivos
+    regex = re.compile(r"(MM|HH)h?(m(h?))*")
+
+    validacao = regex.fullmatch(arranjo)
+
+    return bool(validacao)
+
+def g(arranjo):
+    # O regex verifica se o arranjo familiar possui adultos dentro dos limites das variáveis x e y com qualquer número de filhos
+    # A expressão não pode aceitar que os três últimos filhos sejam homens
+    x = int(input("Coloque um valor para o número x: "))
+    y = int(input("Coloque um valor para o número y: "))
+
+    # Laço para garantir que os valores de x e y sejam: x > 0, y > 0 e x <= y
+    while(x < 0 or y < 0 or x > y):
+        if(x < 0):
+            x = int(input("Coloque outro valor para x que seja maior que 0: "))
+        if(y < 0):
+            y = int(input("Coloque outro valor para y que seja maior que 0: "))
+        if(x > y):
+            x = int(input("Coloque outro valor para x que seja menor ou igual que o de y: "))
+    regex = re.compile(f"(H|M){{{x},{y}}}(((h|m)*(m|mh|mhh))|(h|hh))?")
+
+    validacao = regex.fullmatch(arranjo)
+
+    return bool(validacao)
---
+++ teste.py
@@ -0,0 +1,34 @@
+import funcoes
+
+# Cadeias de teste para o estresse das funções
+nomes_teste = ["Eduardo Albuquerque", "Jo@o Pedr0 V@sconcelo$", "Ad E T", "Marcelo melo", "V1rg1l1o N3t0", "Ladielma Carina Santos Teixeira",  "caroline", "Jonas1 Tomilho", "Calandrinix #nias", "alina Sardinha", "sANDRA cASSANDRA", "Jonas Dantas Teixeira", "Joao Lucas", "CarlosAugusto", "Mauricio Cardoso Silveira"]
+
+emails_teste = ["lianC@gmail.com.br", "AnyIla@com.br", "123.com.br","@gmail.com.br", "anivcina@hotmail.com.br", "@oter.com.br", "##@ctrl.com.br", "andre@ufpa.br", "ana@hotmail.br", "cptx@word.com.br", "dani@brs.com", "SZA@GMAIL.COM.BR", "@gmail.com.br", "mandila@ufpa.br","will@gmail.br"]
+
+senhas = ["kvy487", "Q3ut9", "LEtSPK4d", "su1672wm", "nNOyDdL8", "t1.uqXcfc", "np9x7NoB", "DBUaCJ05", "DEGes2t31v", "kXS+8r5u", "KSR43ED4", "kcs9.d'aa", "Ets493Qs", "okgv990c", "JDSJAISS"]
+
+cpfs = ["952.054.440-29", "555.635.190-23", "37681410040", "51.728.580-09", "407.182.170-125", "491.623.160-01", "187.673.420-02", "9l8.479.910-73", "345.728.830-06", "aLS.As@.^sf.dd", "213.437.864.55", "012.781.996", "934-604-049-72", "018.658.852-66","299.556.043-92"]
+
+telefones = ["(61) 92065-1842", "83000938073770", "(83) 932640687", "5593752-3542", "63 935140454", "69927105345", "(85)82722-0241", "(539) 92714-3575", "62 92896-0044", "(73)92663-9162", "(85) 92931-37118", "(28)936067596", "32 9434-900", "(51) 99423-7768", "91 98349-0032"]
+
+datas = ["2017-06-26 19:36:14", "20/07/2012 14:05:51", "04/09/1970 09:11:01", "02/10/2022/ 01:28:08", "11/16/2016 8:9:5", "03.26.1974 18:38:43", "14:57:14 05/06/1997", "07/15/1971-05/03/52", "07/25/198318:12:39", "06/22/ 1979 03:42:21", "12/13/1980 03:25:30", "03/05/1991 17:04:02", "94/15/1275 72:43:34", "21/02/12 12:33:21", "24/12/3124 51:32:12"]
+
+numeros = ["18", "-83", "-92,69", "+8.9", "-32.0", ".1", "a", "+68", "12.-4", "+", "-156.66516", "0", "-23.33", "+@.#", "132."]
+
+arranjos_a = ["HMhhhm", "MMmhm", "Mhm", "MHh", "HHmhmhmh", "MHMhh", "MHmhmhmmhh", "MM", "MH", "HMhhmmh", "MMhhh", "HHmmmmh","MHmmhhhhhh","MHMH","Hmmh"]
+
+arranjos_b = ["MHmmmh", "MHhhmmh", "HMmhmhmhh", "Mhhm", "HMmhmmmmhmhm", "MHm", "MMmmm", "MMhhhmmmh", "HMmmh", "HMmmhhm", "mmhhhhm", "HHmmhhhhh", "MHmhmhmhhmhmhmhmhhmmhhmm", "HMmhhm", "HMMHmmmh"]
+
+arranjos_c = ["HMmhhmh", "HMhhmhhm", "MHmhhhmmh", "HHmmmh", "HHMhhmmhm", "HMmh", "MMmhmhmmh", "MHhmmhh", "MHmhmmh", "HHHmmhh", "HMmmh", "hmm", "Mmmh", "HMhmh", "MHmmmhhh"]
+
+arranjos_d = ["HHmhhhmmhmh", "MMhhm", "MHhmhhmhmmh", "HHMhmmhh", "MMmhhhmmhmhm", "HHmh", "HHmmhhmhh", "MMhmhhmmmhhm", "MHhmhhmmhmhmmhm", "MMhmmhhmmmmmmmhhmmh", "HHmmmmmmmmmm", "HHHmhmmmmmhm", "HH", "MMhmhmhmhm", "HMhhhmmmhh"]
+
+arranjos_e = ["HHmhhmh", "MMmhmhm", "HMhmhmhhh", "MMhmhmhmh", "MMhhmhhmhh", "MMhmhmhmhmhmhmhmhmh", "HHmh", "MH", "MHhmhmhm", "MMhmhmh", "HHmhmhh", "MMhmhmhmhm", "HHHHmhmh", "MM", "MHmh"]
+
+arranjos_f = ["HHmhmhmmhhmhm", "MMmmmmmmm", "HHmhhmhhmmhhmmmhhmmhh", "HHhmmmmmmmmmmmh", "MMhmhhmh", "HMhmmh", "HHhmhmhhmmh", "MMh", "MMmmhmmmhmmmmh", "MMhm", "HHhhhhhhhh", "MMhmhhh", "HHmhmhm", "HH", "HMmmhmhmhmm"]
+
+arranjos_g = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+
+
+#chamada das funções para a execução dos testes
+funcoes.teste_estresse(arranjos_d, funcoes.g)
